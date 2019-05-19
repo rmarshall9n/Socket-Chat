70195d0dbfd4752f1864c630ec04e7dc6d6af576
Ryan Marshall <rmarshall9n@gmail.com>
2019-05-19 12:56:06 +0100

    Added basic message sending with feedback.

--- index.js
+++ index.js
@@ -11,5 +11,11 @@ app.use(express.static("public"));
 var io = socket(server);
 
 io.on("connection", function(socket) {
-  console.log("connected", socket.id);
+  socket.on("chat", function(data) {
+    io.sockets.emit("chat", data);
+  });
+
+  socket.on("typing", function(data) {
+    socket.broadcast.emit("typing", data);
+  });
 });
--- public/app.js
+++ public/app.js
@@ -1,1 +1,31 @@
 var socket = io.connect("http://localhost:3000/");
+
+var message = document.getElementById("message"),
+  output = document.getElementById("output"),
+  handle = document.getElementById("handle"),
+  feedback = document.getElementById("feedback"),
+  send = document.getElementById("send");
+
+send.addEventListener("click", function() {
+  socket.emit("chat", {
+    message: message.value,
+    handle: handle.value
+  });
+  message.value = "";
+});
+
+message.addEventListener("keydown", function() {
+  socket.emit("typing", {
+    handle: handle.value
+  });
+});
+
+socket.on("chat", function(data) {
+  feedback.innerHTML = "";
+  output.innerHTML +=
+    "<p><em>" + data.handle + ":</em> " + data.message + "</p>";
+});
+
+socket.on("typing", function(data) {
+  feedback.innerHTML = "<p><em>" + data.handle + " is typing...</em></p>";
+});
